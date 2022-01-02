fbd3fb20751a1b2eca91bd3d045cb5bf337d3757
Tushar Samant <live@now-is.org>
2022-01-01 23:55:05 -0600

    Paint multiple digits

--- rectangle.lua
+++ rectangle.lua
@@ -34,6 +34,35 @@ local function paint_rectangle (scr, l, c, rect)
 	end
 end
 
+local function rectangle_width (rect)
+	local width = 0
+	for _, line in ipairs(split_on_char('\n', rect)) do
+		if width < #line then
+			width = #line
+		end
+	end
+	return width
+end
+
+local function paint_rectangles (scr, l, c, rects, space)
+	local width
+	for _, rect in ipairs(rects) do
+		paint_rectangle(scr, l, c, rect)
+		c = c + rectangle_width(rect) + space
+	end
+end
+
+local function rectangles_of (str, fill_char)
+	local rectangles = {}
+	for c in str:gmatch('.') do
+		local d = LED.digit(tonumber(c), fill_char)
+		if d ~= nil then
+			rectangles[#rectangles+1] = d
+		end
+	end
+	return rectangles
+end
+
 C.initscr()
 -- disable line buffering
 C.cbreak()
@@ -42,16 +71,22 @@ C.echo(false)
 local scr = C.stdscr()
 scr:nodelay(true)
 
-while true do
-  scr:clear()
-  paint_rectangle(scr, 1, 10, LED.digit(os.time() % 10, '@'))
-  scr:move(0, 0)
-  scr:refresh()
+local quit_char = string.byte('q')
 
-  if scr:getch() == 113 then -- 'q'
-	  break
-  end
-  sleep(1000)
+while true do
+	scr:clear()
+	local sec = tostring(os.time() % 60)
+	if #sec < 2 then
+		sec = '0' .. sec
+	end
+	paint_rectangles(scr, 5, 10, rectangles_of(sec, '@'), 6)
+	scr:move(0, 0)
+	scr:refresh()
+
+	if scr:getch() == quit_char then
+		break
+	end
+	sleep(1000)
 end
 
 -- free curses memory
